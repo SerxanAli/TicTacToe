f900f5a5cffab64fab4169d0576d383a5348fb68
SerxanAli <aliyev.s.v1990@gmail.com>
2023-04-07 14:56:37 +0400

    1.2

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -37,22 +37,79 @@ class ViewController: UIViewController {
     
     var NOUGHT = "O"
     var CROSS  = "X"
-    
+    var board = [UIButton]()
     
     // MARK: - FUNCTIONS
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setInitViews()
     }
 
-    @IBAction func boardTapAction(_ sender: UIButton) {
+    private func setInitViews(){
         
-        addToBoard(sender)
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
+        
         
     }
     
-    func addToBoard(_ sender: UIButton) {
+    
+    @IBAction func boardTapAction(_ sender: UIButton) {
+        addToBoard(sender)
+        
+        if fullBoard() {
+            resultAlert(title: "Draw")
+        }
+    }
+    
+    private func resultAlert(title:String) {
+        
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { _ in
+            self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+
+    private func resetBoard(){
+        
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        
+        if firstTurn == Turn.Nought {
+            firstTurn = Turn.Cross
+            turnLabel.text = CROSS
+        }else
+        if firstTurn == Turn.Cross {
+            firstTurn = Turn.Nought
+            turnLabel.text = NOUGHT
+        }
+        
+        currentTurn = firstTurn
+        
+    }
+    
+    private func fullBoard() -> Bool {
+        for button in board {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
+    }
+    
+   private func addToBoard(_ sender: UIButton) {
         if sender.title(for: .normal) == nil {
             if currentTurn == Turn.Nought {
                 sender.setTitle(NOUGHT, for: .normal)
